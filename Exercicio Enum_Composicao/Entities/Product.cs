acb0b5f99102b130f93a531e20ed69c836777c71
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 21:24:55 -0300

    Criacao classe Product

---
+++ Exercicio Enum_Composicao/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Enum_Composicao.Entities {
+    class Product {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+    }
+}
