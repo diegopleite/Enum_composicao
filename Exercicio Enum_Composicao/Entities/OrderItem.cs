bb693ec2cd6f0c015a3e6c5dd35ac4ef951a2872
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 21:40:15 -0300

    Criacao classe OrderItem

---
+++ Exercicio Enum_Composicao/Entities/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Enum_Composicao.Entities {
+    class OrderItem {
+        public int Quantity { get; private set; }
+        public double Price { get; private set; }
+        public Product Product { get; private set; }
+
+        public OrderItem(int quantity, double price, Product product) {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double subTotal() {
+            return Quantity * Price;
+          
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Quantity);
+            sb.Append(Price);
+            sb.Append(Product.Name);
+            return sb.ToString();
+        }
+    }
+}
