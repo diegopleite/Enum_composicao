89bc90247607ffe7f4a8d82baedfa7280ed32d1f
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-30 21:17:20 -0300

    iniciando projeto

---
+++ Exercicio Enum_Composicao/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Exercicio_Enum_Composicao {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
